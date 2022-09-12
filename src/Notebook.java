00b6be39629bf97dcb1027eeaaed3c33042c0244
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-12 17:50:42 +0600

    This is our code

--- src/Notebook.java
+++
@@ -1,7 +0,0 @@
-public class Notebook extends Phone{
-    String prosesor;
-
-    public Notebook(String model, String prosesor, int weight, String color, int memory) {
-        super(model, weight, color, memory);
-    }
-}
--- src/Tv.java
+++
@@ -1,5 +0,0 @@
-public class Tv extends Phone{
-    public Tv(String model, int diametr, String color,int godvpuska) {
-        super(model, diametr, color,godvpuska);
-    }
-}
